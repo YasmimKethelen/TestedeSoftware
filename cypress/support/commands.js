32c609710db6bbf5071ae6e0ee7c6404c40d7bdc
yasmim.silva <yasmim.silva@fpf.br>
2025-07-18 20:02:59 -0400

    commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('loginStandardUser', () => {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+});
